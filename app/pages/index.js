6cd708553c6118827773462ffe7828c0eca4331b
Lorien Cho <lorien.yuna.cho@gmail.com>
2024-10-07 23:52:12 -0400

    working on styling

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,15 +1,8 @@
-require('dotenv').config();
 import { FileInput } from "./FileInput.tsx";
-import callNIKLAPI from "./callNIKLAPI.js";
-import translateText from "./translateText.js";
-
 export default function App() {
-  async function translate() {
-    const translated = await translateText("");
-  }
   return (
     <div className="App">
-      <h1 onClick={translate}>Custom File Input</h1>
+      <h1>Custom File Input</h1>
       <FileInput />
     </div>
   );
